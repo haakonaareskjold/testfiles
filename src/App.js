75e0b9a7e1b2ee9f818038d80f7e9e5f6417d86a
Haakon36 <aareskjold.haakon@gmail.com>
2019-11-07 15:34:03 +0100

    updating app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1>Haakon's website for lidl tests</h1>
+        <p>Every component will be divided by a header</p>
+        <br />
+        <hr />
+        <br />
         <Counter />
         <br />
         <hr />
