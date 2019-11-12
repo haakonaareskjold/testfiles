68fcbca119d264215acc3d7682a1dd9d91ab1570
Haakon36 <aareskjold.haakon@gmail.com>
2019-11-12 16:56:39 +0100

    fixed width and height inline in iframe videoplayer

--- src/videoPlayer/VideoPlayer.js
+++ src/videoPlayer/VideoPlayer.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Menu from './menu';
+import React from "react";
+import Menu from "./menu";
 
 const VIDEOS = {
-  RickRoll: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1',
-  HEYAYAYA: 'https://www.youtube.com/embed/cVYvozAWPtc?autoplay=1',
-  Kappa: 'https://www.youtube.com/embed/BpHSm0KcW7o?autoplay=1',
-  HL3Trailer: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
+  RickRoll: "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1",
+  HEYAYAYA: "https://www.youtube.com/embed/cVYvozAWPtc?autoplay=1",
+  Kappa: "https://www.youtube.com/embed/BpHSm0KcW7o?autoplay=1",
+  HL3Trailer: "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
 };
 
 export default class VideoPlayer extends React.Component {
@@ -14,20 +14,23 @@ export default class VideoPlayer extends React.Component {
     this.chooseVideo = this.chooseVideo.bind(this);
     this.state = { src: VIDEOS.fast };
   }
-  
+
   chooseVideo(newVideo) {
-  this.setState({
-    src: VIDEOS[newVideo]
-  });
-}
-  
+    this.setState({
+      src: VIDEOS[newVideo]
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Pick a video to make it play</h1>
-         <Menu chooseVideo={this.chooseVideo} />
-         <iframe title ="Video" style = {{width: "720", height: "480", border: "none", frameborder: "0"}} 
-        src={this.state.src} />
+        <Menu chooseVideo={this.chooseVideo} />
+        <iframe
+          title="Video"
+          style={{ width: 720, height: 480, border: "none", frameborder: 0 }}
+          src={this.state.src}
+        />
       </div>
     );
   }
